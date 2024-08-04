2a5c8376aecf3840b8683ff50dae30a72a6595b0
chenfei2008c@icloud.com <chenfei2008c@icloud.com>
2024-08-05 03:50:19 +0800

    commit

--- FlapPyBird-master/main.py
+++
@@ -1,6 +0,0 @@
-import asyncio
-
-from src.flappy import Flappy
-
-if __name__ == "__main__":
-    asyncio.run(Flappy().start())
--- FlapPyBird-master/src/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .game_config import GameConfig
-from .images import Images
-from .sounds import Sounds
-from .utils import clamp, get_hit_mask, pixel_collision
-from .window import Window
